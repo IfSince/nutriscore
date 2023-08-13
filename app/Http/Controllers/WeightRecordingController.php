7ed21c647daed3603adbbb250b5c886776ef3bf6
IfSince <leonlaade@gmx.de>
2023-08-13 20:00:53 +0200

    use weight recording resource

--- app/Http/Controllers/WeightRecordingController.php
+++ app/Http/Controllers/WeightRecordingController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\WeightRecordingRequest;
+use App\Http\Resources\WeightRecordingResource;
 use App\Models\User;
 use App\Models\WeightRecording;
 use App\Services\WeightRecordingService;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Response;
 use Symfony\Component\HttpFoundation\Response as Status;
 
@@ -16,33 +18,33 @@ class WeightRecordingController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index(User $user): Collection {
-        return $user->weightRecordings;
+    public function index(User $user): AnonymousResourceCollection {
+        return WeightRecordingResource::collection($user->weightRecordings);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(WeightRecordingRequest $request, User $user): WeightRecording {
-        return $this->weightRecordingService->create($request->validated(), $user);
+    public function store(WeightRecordingRequest $request, User $user): WeightRecordingResource {
+        return WeightRecordingResource::make($this->weightRecordingService->create($request->validated(), $user));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(WeightRecording $weightRecording): WeightRecording {
-        return $weightRecording;
+    public function show(WeightRecording $weightRecording): WeightRecordingResource {
+        return WeightRecordingResource::make($weightRecording);
     }
 
-    public function showLatest(User $user): WeightRecording {
-        return $user->currentWeightRecording;
+    public function showLatest(User $user): WeightRecordingResource {
+        return WeightRecordingResource::make($user->currentWeightRecording);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(WeightRecordingRequest $request, WeightRecording $weightRecording): WeightRecording {
-        return $this->weightRecordingService->update($request->validated(), $weightRecording);
+    public function update(WeightRecordingRequest $request, WeightRecording $weightRecording): WeightRecordingResource {
+        return WeightRecordingResource::make($this->weightRecordingService->update($request->validated(), $weightRecording));
     }
 
     /**
